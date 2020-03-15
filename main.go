c6537326335e528c65b65eb71cfea8427acfac63
Rassul <rasul.tabulov@gmail.com>
2020-03-15 13:14:41 +0600

    updates

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
+func main() {
+	// static folder
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	port = ":" + port
+
+	log.Print("Listening on http://localhost", port, "\n")
+	log.Fatal(http.ListenAndServe(port, nil))
 }
